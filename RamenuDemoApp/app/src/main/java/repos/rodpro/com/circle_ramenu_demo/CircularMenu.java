7e1858a45dd682cab460f0409a0bf8a1b89477f2
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-11-06 09:40:22 -0500

    Add more images

---
+++ RamenuDemoApp/app/src/main/java/repos/rodpro/com/circle_ramenu_demo/CircularMenu.java
@@ -0,0 +1,16 @@
+package repos.rodpro.com.circle_ramenu_demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CircularMenu extends AppCompatActivity {
+    private static final String TAG = "CIRCLE_MENU_B";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circular_menu);
+    }
+
+}
--- RamenuDemoApp/app/src/main/java/repos/rodpro/com/circle_ramenu_demo/MainActivity.java
+++ RamenuDemoApp/app/src/main/java/repos/rodpro/com/circle_ramenu_demo/MainActivity.java
@@ -1,14 +1,69 @@
 package repos.rodpro.com.circle_ramenu_demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ramotion.circlemenu.CircleMenuView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "CIRCLE_MENU";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureCircleMenu();
+    }
+
+    private void configureCircleMenu() {
+        final CircleMenuView menu = findViewById(R.id.center_menu);
+        menu.setEventListener(new CircleMenuView.EventListener() {
+            @Override
+            public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
+                Log.d(TAG, "onMenuOpenAnimationStart");
+            }
+
+            @Override
+            public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d(TAG, "onMenuOpenAnimationEnd");
+            }
+
+            @Override
+            public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
+                Log.d(TAG, "onMenuCloseAnimationStart");
+            }
+
+            @Override
+            public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d(TAG, "onMenuCloseAnimationEnd");
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int index) {
+                Log.d(TAG, "onButtonClickAnimationStart| index: " + index);
+            }
+
+            @Override
+            public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int index) {
+                Log.d(TAG, "onButtonClickAnimationEnd| index: " + index);
+                processOptionSelected(index);
+            }
+        });
+    }
+
+    private void processOptionSelected(int option) {
+        switch (option){
+            case 0:
+                launchCircleMenu();
+        }
+    }
+
+    private void launchCircleMenu() {
+        Intent intent = new Intent(this, CircularMenu.class);
+        startActivity(intent);
     }
 }
